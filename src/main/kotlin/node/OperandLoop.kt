11337840c35cce9d144b6e3c4b77e0ddbe634e4a
Daniil <97092438+danyazero@users.noreply.github.com>
2025-12-02 20:17:27 +0200

    Provide operand loop refactoring

--- src/main/kotlin/node/OperandLoop.kt
+++ src/main/kotlin/node/OperandLoop.kt
@@ -1,14 +1,11 @@
 package com.danyazero.node
 
-import com.danyazero.expression.LessExpression
 import com.danyazero.expression.ValueExpression
 import com.danyazero.model.Expression
 import com.danyazero.model.Node
+import com.danyazero.model.UntilRange
 import com.danyazero.type.ArrayType
-import com.danyazero.type.IntegerType
 import com.danyazero.utils.GenerationContext
-import org.objectweb.asm.Label
-import org.objectweb.asm.Opcodes
 
 class OperandLoop(
     val operand: Expression,
@@ -21,36 +18,13 @@ class OperandLoop(
         val operandType = operand.getType(ctx)
         if (operandType !is ArrayType) throw RuntimeException("Operand loop can accept only arrays")
 
-        val enterLabel = Label()
-        val exitLabel = Label()
-        ctx.enterScope()
-
-        Variable(index, IntegerType(), ValueExpression.of(0)).produce(ctx)
-        ctx.defineVariable(item, operandType.child)
-
-        ctx.getMethodVisitor().visitLabel(enterLabel)
-
-        val conditionExpression = LessExpression(
-            left = Operand(index, true),
-            right = Length(operand),
-        )
-        conditionExpression.target(exitLabel)
-        conditionExpression.produce(ctx)
-
-        Variable(item, operandType.child, ArrayValue(operand = operand, index = Operand(index, true))).produce(ctx)
-
-
-        statements.forEach { it.produce(ctx) }
-
-        val indexVariable = ctx.resolveLocalVariable(index)
-            ?: throw RuntimeException("Loop index resolve exception")
-
-        IntegerType().inc(ctx.getMethodVisitor(), indexVariable.index, 1)
-
-        ctx.getMethodVisitor().visitJumpInsn(Opcodes.GOTO, enterLabel)
-        ctx.exitScope()
-        ctx.getMethodVisitor().visitLabel(exitLabel)
-
+        val arrayItem = ArrayValue(operand = operand, index = Operand(index, true))
+        val itemVariable = Variable(name = item, type = operandType.child, value = arrayItem)
+        RangeLoop(
+            range = UntilRange(start = ValueExpression.of(0), end = Length(operand)),
+            index = index,
+            statements = listOf(itemVariable) + statements
+        ).produce(ctx)
     }
 
     override fun toString(): String {
